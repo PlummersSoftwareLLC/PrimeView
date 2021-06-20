7a0246ed7494a9dcb11882b877a366f1728e1ee3
Rutger van Bergen <rbergen@xs4all.nl>
2021-06-20 15:56:43 +0200

    Sync with upstream

---
+++ src/Frontend/Pages/ReportDetails.razor.cs
@@ -0,0 +1,72 @@
+﻿using BlazorTable;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using PrimeView.Entities;
+using PrimeView.Frontend.Types;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeView.Frontend.Pages
+{
+	public partial class ReportDetails
+	{
+		[Inject]
+		public HttpClient Http { get; set; }
+
+		[Inject] 
+		public IReportReader ReportReader { get; set; }
+
+		[Parameter]
+		public string ReportId { get; set; }
+
+		private string ReportTitle
+		{
+			get
+			{
+				StringBuilder titleBuilder = new();
+
+				if (report?.User != null)
+					titleBuilder.Append($" by {report.User}");
+
+				if (report?.Date != null)
+					titleBuilder.Append($" at {report.Date.Value.ToLocalTime()}");
+
+				return titleBuilder.Length > 0 ? $"Report generated{titleBuilder}" : "Report";
+			}
+		}
+
+		private Table<Result> resultTable;
+		private Report report = null;
+		private int rowNumber = 0;
+		private Dictionary<string, LanguageInfo> languageMap = null;
+
+		protected override async Task OnInitializedAsync()
+		{
+			report = await ReportReader.GetReport(ReportId);
+			await LoadLanguageMap();
+		}
+
+		private async Task LoadLanguageMap()
+		{
+			try
+			{
+				languageMap = await Http.GetFromJsonAsync<Dictionary<string, LanguageInfo>>("data/langmap.json");
+				foreach (var entry in languageMap)
+					entry.Value.Key = entry.Key;
+			}
+			catch	{}
+		}
+
+		private void ResetRowNumber()
+		{
+			rowNumber = resultTable.PageNumber * resultTable.PageSize;
+		}
+
+		private LanguageInfo GetLanguageInfo(string language)
+			=> languageMap != null && languageMap.ContainsKey(language) ? languageMap[language] : new() { Key = language, Name = language[0].ToString().ToUpper() + language[1..] };
+	}
+}
---
+++ src/Frontend/Types/LanguageInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimeView.Frontend.Types
+{
+	public class LanguageInfo
+	{
+		public string Key { get; set; }
+		public string Name { get; set; }
+		public string URL { get; set; }
+
+		public class KeyEqualityComparer : IEqualityComparer<LanguageInfo>
+		{
+			public bool Equals(LanguageInfo x, LanguageInfo y)
+				=> (x == null && y == null) || (x != null && y != null && x.Key == y.Key);
+
+			public int GetHashCode([DisallowNull] LanguageInfo obj)
+				=> obj.Key.GetHashCode();
+		}
+	}
+}
