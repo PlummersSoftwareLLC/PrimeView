33d1788db38e05bd66c4f994e75926a403f36861
Rutger van Bergen <rbergen@xs4all.nl>
2021-06-20 21:04:08 +0200

    Normalized language names

---
+++ src/Frontend/Types/LanguageInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimeView.Frontend.Types
+{
+	public class LanguageInfo
+	{
+		public string Name { get; set; }
+		public string URL { get; set; }
+	}
+}
