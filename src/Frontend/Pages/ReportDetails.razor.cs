3d94bd6e3def774c032d0266816dd7fa7d5f3658
Rutger van Bergen <rbergen@xs4all.nl>
2021-06-27 11:49:25 +0200

    Basic filtering complete

--- src/Frontend/Pages/ReportDetails.razor.cs
+++ src/Frontend/Pages/ReportDetails.razor.cs
@@ -1,6 +1,7 @@
 ﻿using Blazored.LocalStorage;
 using BlazorTable;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Components.Rendering;
 using Microsoft.JSInterop;
 using PrimeView.Entities;
@@ -26,7 +27,7 @@ namespace PrimeView.Frontend.Pages
 		[Inject]
 		public ISyncLocalStorageService LocalStorage { get; set; }
 
-		[Inject] 
+		[Inject]
 		public IReportReader ReportReader { get; set; }
 
 		[Inject]
@@ -46,6 +47,91 @@ namespace PrimeView.Frontend.Pages
 
 		[QueryStringParameter("id")]
 		public string ReportId { get; set; }
+
+		[QueryStringParameter("fi")]
+		public string FilterImplementationText { get; set; } = string.Empty;
+
+		[QueryStringParameter("fp")]
+		public string FilterParallelismText
+		{
+			get => GetFilterValueString(!FilterParallelSinglethreaded, "st", !FilterParallelMultithreaded, "mt");
+
+			set
+			{
+				value = $"~{value}~";
+
+				FilterParallelSinglethreaded = !value.Contains("~st~");
+				FilterParallelMultithreaded = !value.Contains("~mt~");
+			}
+		}
+
+		[QueryStringParameter("fa")]
+		public string FilterAlgorithmText
+		{
+			get => GetFilterValueString(!FilterAlgorithmBase, "ba", !FilterAlgorithmWheel, "wh", !FilterAlgorithmOther, "ot");
+
+			set
+			{
+				value = $"~{value}~";
+
+				FilterAlgorithmBase = !value.Contains("~ba~");
+				FilterAlgorithmWheel = !value.Contains("~wh~");
+				FilterAlgorithmOther = !value.Contains("~ot~");
+			}
+		}
+
+		[QueryStringParameter("ff")]
+		public string FilterFaithfulText
+		{
+			get => GetFilterValueString(!FilterFaithful, "ff", !FilterUnfaithful, "uf");
+
+			set
+			{
+				value = $"~{value}~";
+
+				FilterFaithful = !value.Contains("~ff~");
+				FilterUnfaithful = !value.Contains("~uf~");
+			}
+		}
+
+		[QueryStringParameter("fb")]
+		public string FilterBitsText
+		{
+			get => GetFilterValueString(!FilterBitsUnknown, "uk", !FilterBitsOne, "on", !FilterBitsOther, "ot");
+
+			set
+			{
+				value = $"~{value}~";
+
+				FilterBitsUnknown = !value.Contains("~uk~");
+				FilterBitsOne = !value.Contains("~on~");
+				FilterBitsOther = !value.Contains("~ot~");
+			}
+		}
+
+		public IList<string> FilterImplementations
+			=> FilterImplementationText.Split("~", StringSplitOptions.RemoveEmptyEntries);
+
+		public bool FilterParallelSinglethreaded { get; set; } = true;
+		public bool FilterParallelMultithreaded { get; set; } = true;
+
+		public bool FilterAlgorithmBase { get; set; } = true;
+		public bool FilterAlgorithmWheel { get; set; } = true;
+		public bool FilterAlgorithmOther { get; set; } = true;
+
+		public bool FilterFaithful { get; set; } = true;
+		public bool FilterUnfaithful { get; set; } = true;
+
+		public bool FilterBitsUnknown { get; set; } = true;
+		public bool FilterBitsOne { get; set; } = true;
+		public bool FilterBitsOther { get; set; } = true;
+
+		private bool AreFiltersClear
+			=> FilterImplementationText == string.Empty
+				&& FilterParallelismText == string.Empty
+				&& FilterAlgorithmText == string.Empty
+				&& FilterFaithfulText == string.Empty
+				&& FilterBitsText == string.Empty;
 
 		private string ReportTitle
 		{
@@ -68,19 +154,8 @@ namespace PrimeView.Frontend.Pages
 		private int rowNumber = 0;
 		private Dictionary<string, LanguageInfo> languageMap = null;
 		private bool processTableSortingChange = false;
-		private string fi 
-		{ 
-			get => fi_internal;
-			set
-			{
-				Console.WriteLine(value);
-				fi_internal = value;
-			}
-		}
-		private string fi_internal;
-		private bool fp_st = true, fp_mt = true;
-		private bool fa_ba = true, fa_wh = true, fa_ot = true;
-		private bool fb_uk = true, fb_on = true, fb_ot = true;
+
+		private ElementReference implementationsSelect;
 
 		protected override async Task OnInitializedAsync()
 		{
@@ -98,11 +173,9 @@ namespace PrimeView.Frontend.Pages
 		protected override async Task OnAfterRenderAsync(bool firstRender)
 		{
 			(string sortColumn, bool sortDescending) = resultTable.GetSortParameterValues();
-			Console.WriteLine($"OnAfterRenderAsync: GetSortParameterValues: sortColum = {sortColumn}, sortDescending = {sortDescending}");
 
 			if (!processTableSortingChange && (!SortColumn.EqualsIgnoreCaseOrNull(sortColumn) || SortDescending != sortDescending))
 			{
-				Console.WriteLine($"SetSortParameterValues: SortColum = {SortColumn}, SortDescending = {SortDescending}");
 				if (resultTable.SetSortParameterValues(SortColumn, SortDescending))
 					await resultTable.UpdateAsync();
 			}
@@ -110,6 +183,17 @@ namespace PrimeView.Frontend.Pages
 			UpdateQueryString();
 
 			await base.OnAfterRenderAsync(firstRender);
+		}
+
+		private async Task ClearFilters()
+		{
+			FilterImplementationText = string.Empty;
+			FilterParallelismText = string.Empty;
+			FilterAlgorithmText = string.Empty;
+			FilterFaithfulText = string.Empty;
+			FilterBitsText = string.Empty;
+
+			await JSRuntime.InvokeVoidAsync("PrimeViewJS.ClearMultiselectValues", implementationsSelect);
 		}
 
 		private void ToggleSystemInfoPanel()
@@ -145,7 +229,6 @@ namespace PrimeView.Frontend.Pages
 				return;
 
 			(string sortColumn, bool sortDescending) = resultTable.GetSortParameterValues();
-			Console.WriteLine($"OnTableRefreshStart: GetSortParameterValues: sortColum = {sortColumn}, sortDescending = {sortDescending}");
 
 			processTableSortingChange = false;
 
@@ -164,10 +247,7 @@ namespace PrimeView.Frontend.Pages
 			}
 
 			if (queryStringUpdateRequired)
-			{
-				Console.WriteLine($"UpdateQueryString: SortColum = {SortColumn}, SortDescending = {SortDescending}");
 				UpdateQueryString();
-			}
 		}
 
 		private LanguageInfo GetLanguageInfo(string language)
@@ -175,5 +255,23 @@ namespace PrimeView.Frontend.Pages
 
 		private void UpdateQueryString()
 			=> this.UpdateQueryString(NavigationManager, LocalStorage, JSRuntime);
+
+		private async Task ImplementationSelectionChanged(EventArgs args)
+		{
+			FilterImplementationText = await JSRuntime.InvokeAsync<string>("PrimeViewJS.GetMultiselectValues", implementationsSelect, "~") ?? string.Empty;
+		}
+
+		private string GetFilterValueString(params object[] flagSet)
+		{
+			List<string> setFlags = new(); 
+
+			for (int i = 0; i < flagSet.Length; i += 2)
+			{
+				if ((bool)flagSet[i])
+					setFlags.Add(flagSet[i + 1].ToString());
+			}
+
+			return setFlags.Count > 0 ? string.Join("~", setFlags) : string.Empty;
+		}
 	}
 }
