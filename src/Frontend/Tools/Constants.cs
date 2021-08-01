3d2b64617de889807752825b3b9494b33473cb7f
Rutger van Bergen <rbergen@xs4all.nl>
2021-08-01 12:44:12 +0200

    Clear pages branch

--- src/Frontend/Tools/Constants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PrimeView.Frontend.Tools
-{
-	public static class Constants
-	{
-		public const string ActiveReader = nameof(ActiveReader);
-		public const string Readers = nameof(Readers);
-		public const string JsonFileReader = nameof(JsonFileReader);
-	}
-}
