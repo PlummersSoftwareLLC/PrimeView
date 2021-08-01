64c1b2d3b945411b75332af79ccc541429868d53
rbergen <rbergen@xs4all.nl>
2021-08-01 11:29:00 +0000

    Deploying to pages from @ PlummersSoftwareLLC/PrimeView@154cc6b0c8ace7e958cda4debe063057c7ef88a8 🚀

---
+++ js/app.js
@@ -0,0 +1,29 @@
+﻿var PrimeViewJS = PrimeViewJS || {};
+
+PrimeViewJS.ShowUrl = function (url) {
+	window.history.pushState(null, "", url);
+};
+
+PrimeViewJS.GetMultiselectValues = function (element, valueSeparator) {
+	var selectedValues = [];
+
+	for (var i = 0; i < element.length; i++) {
+		if (element.options[i].selected)
+			selectedValues.push(element.options[i].value);
+	}
+
+	return selectedValues.length > 0 ? selectedValues.join(valueSeparator) : null;
+};
+
+PrimeViewJS.ClearMultiselectValues = function (element) {
+	for (var i = 0; i < element.length; i++) {
+		if (element.options[i].selected)
+			element.options[i].selected = false;
+	}
+};
+
+PrimeViewJS.SetMultiselectValues = function (element, values) {
+	for (var i = 0; i < element.length; i++) {
+		element.options[i].selected = values.includes(element.options[i].value);
+	}
+};
