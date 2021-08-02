b2f1cb9d596448d1745bfb6ba476c0e78eab7ef0
Rutger van Bergen <rbergen@xs4all.nl>
2021-08-02 20:41:52 +0200

    Index page updates

---
+++ src/Frontend/Pages/Index.razor.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Configuration;
+using PrimeView.Entities;
+using PrimeView.Frontend.Parameters;
+using PrimeView.Frontend.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimeView.Frontend.Pages
+{
+	public partial class Index
+	{
+		[Inject]
+		public IConfiguration Configuration { get; set; }
+
+		[Inject]
+		public IReportReader ReportReader { get; set; }
+
+		[QueryStringParameter("rc")]
+		public int MaxReportCount { get; set; }
+
+		private ReportSummary[] summaries = null;
+		private int newMaxReportCount; 
+
+		public override Task SetParametersAsync(ParameterView parameters)
+		{
+			MaxReportCount = Configuration.GetValue(Constants.MaxReportCount, 30);
+			SortColumn = "dt";
+			SortDescending = true;
+
+			return base.SetParametersAsync(parameters);
+		}
+
+		protected override async Task OnInitializedAsync()
+		{
+			this.summaries = await ReportReader.GetSummaries(MaxReportCount);
+			this.newMaxReportCount = MaxReportCount;
+		}
+
+		private async Task ApplyNewMaxReportCount()
+		{
+			MaxReportCount = newMaxReportCount;
+
+			if (summaries != null)
+				summaries = await ReportReader.GetSummaries(MaxReportCount);
+		}
+
+		private void LoadReport(string reportId)
+		{
+			NavigationManager.NavigateTo($"report?id={reportId}");
+		}
+
+	}
+}
---
+++ src/JsonFileReader/S3BucketIndexReader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace PrimeView.JsonFileReader
+{
+	static class S3BucketIndexReader
+	{
+		public static async Task<string[]?> GetFileNames(HttpClient httpClient)
+		{
+			XElement indexElement;
+
+			try
+			{
+				indexElement = XElement.Parse(await httpClient.GetStringAsync(""));
+			}
+			catch 
+			{
+				return null;
+			}
+
+			XNamespace ns = indexElement.GetDefaultNamespace();
+
+			return indexElement.Descendants(ns + "Contents")?
+				.Select(element => new KeyValuePair<string, DateTime>(element.Element(ns + "Key")!.Value, XmlConvert.ToDateTime(element.Element(ns + "LastModified")!.Value, XmlDateTimeSerializationMode.Utc)))
+				.OrderByDescending(pair => pair.Value)
+				.Select(pair => pair.Key)
+				.ToArray();
+		}
+	}
+}
