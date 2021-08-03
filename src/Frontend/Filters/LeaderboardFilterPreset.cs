ff261b7e1e57f3398168f69c22bac4c2f1d74918
Rutger van Bergen <rbergen@xs4all.nl>
2021-08-03 19:15:53 +0200

    Add option to only show best per language

--- src/Frontend/Filters/LeaderboardFilterPreset.cs
+++ src/Frontend/Filters/LeaderboardFilterPreset.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace PrimeView.Frontend.Filters
+﻿namespace PrimeView.Frontend.Filters
 {
 	public class LeaderboardFilterPreset : ResultFilterPreset
 	{
