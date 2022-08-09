b0c611872c4524c8229ab104f21a11b10da90aaf
Rutger van Bergen <rbergen@xs4all.nl>
2022-08-09 08:50:26 +0200

    Results worksheet is in

--- src/Entities/Result.cs
+++ src/Entities/Result.cs
@@ -1,19 +1,60 @@
-﻿namespace PrimeView.Entities
+﻿using OfficeOpenXml.Attributes;
+using OfficeOpenXml.Table;
+
+namespace PrimeView.Entities
 {
+	[EpplusTable(PrintHeaders = true, ShowTotal = true, TableStyle = TableStyles.Light1)]
 	public class Result
 	{
+		public const int LanguageColumnIndex = 1;
+		[EpplusTableColumn(Order = LanguageColumnIndex, TotalsRowLabel = "Count:")]
 		public string? Language { get; set; }
-		public string? Solution { get; set; }
-		public string? Label { get; set; }
-		public long? Passes { get; set; }
-		public double? Duration { get; set; }
-		public int? Threads { get; set; }
-		public string? Algorithm { get; set; }
-		public bool? IsFaithful { get; set; }
-		public int? Bits { get; set; }
-		public string? Status { get; set; }
-		public double? PassesPerSecond => (double?)Passes / Threads / Duration;
-		public bool IsMultiThreaded => Threads > 1;
 
+		public const int SolutionColumnIndex = 2;
+		[EpplusTableColumn(Order = SolutionColumnIndex, TotalsRowFunction = RowFunctions.Count)]
+		public string? Solution { get; set; }
+
+		public const int SolutionUriColumnIndex = 3;
+		[EpplusTableColumn(Order = SolutionUriColumnIndex, Header = "Solution link")]
+		public string? SolutionUrl { get; set; }
+
+		public const int LabelColumnIndex = 4;
+		[EpplusTableColumn(Order = LabelColumnIndex)]
+		public string? Label { get; set; }
+
+		public const int PassesColumnIndex = 6;
+		[EpplusTableColumn(Order = PassesColumnIndex, Header = "Number of passes")]
+		public long? Passes { get; set; }
+
+		public const int DurationColumnIndex = 7;
+		[EpplusTableColumn(Order = DurationColumnIndex)]
+		public double? Duration { get; set; }
+
+		public const int ThreadsColumnIndex = 8;
+		[EpplusTableColumn(Order = ThreadsColumnIndex, Header = "Number of threads")]
+		public int? Threads { get; set; }
+
+		public const int AlgorithmColumnIndex = 9;
+		[EpplusTableColumn(Order = AlgorithmColumnIndex)]
+		public string? Algorithm { get; set; }
+
+		public const int IsFaithfulColumnIndex = 10;
+		[EpplusTableColumn(Order = IsFaithfulColumnIndex, Header = "Faithful?")]
+		public bool? IsFaithful { get; set; }
+
+		public const int BitsColumnIndex = 11;
+		[EpplusTableColumn(Order = BitsColumnIndex, Header = "Bits per prime")]
+		public int? Bits { get; set; }
+
+		[EpplusIgnore]
+		public string? Status { get; set; }
+
+		public const int PassesPerSecondColumnIndex = 12;
+		[EpplusTableColumn(Order = PassesPerSecondColumnIndex, Header = "Passes / thread / second")]
+		public double? PassesPerSecond => (double?)Passes / Threads / Duration;
+
+		public const int IsMultiThreadedColumnIndex = 5;
+		[EpplusTableColumn(Order = IsMultiThreadedColumnIndex, Header = "Multithreaded?")]
+		public bool IsMultiThreaded => Threads > 1;
 	}
 }
---
+++ src/Frontend/ReportExporters/ExcelConverter.cs
@@ -0,0 +1,53 @@
+﻿using OfficeOpenXml;
+using OfficeOpenXml.Style;
+using PrimeView.Entities;
+using PrimeView.Frontend.Tools;
+using System;
+using System.Drawing;
+
+namespace PrimeView.Frontend.ReportExporters
+{
+	public class ExcelConverter
+	{
+		public static byte[] Convert(Report report, ILanguageInfoProvider languageInfoProvider)
+		{
+			ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+			using ExcelPackage excelPackage = new();
+
+			string tag = $"{report.User ?? string.Empty} {(report.Date.HasValue ? report.Date.Value.ToString() : string.Empty)}".Replace('/', '_').Replace(':', '_');
+			tag = !string.IsNullOrWhiteSpace(tag) ? $" - {tag}" : string.Empty;
+
+			var resultSheet = excelPackage.Workbook.Worksheets.Add($"Results{tag}");
+
+			resultSheet.Cells.LoadFromCollection(report.Results);
+			for (int i = 2; i < resultSheet.Dimension.Rows; i++)
+			{
+				var languageInfo = languageInfoProvider.GetLanguageInfo(resultSheet.Cells[i, Result.LanguageColumnIndex].Text);
+
+				var cell = resultSheet.Cells[i, Result.LanguageColumnIndex];
+				cell.Value = languageInfo.Name;
+
+				if (!string.IsNullOrEmpty(languageInfo.URL))
+				{
+					cell.Hyperlink = new Uri(languageInfo.URL);
+					cell.Style.Font.Color.SetColor(Color.Blue);
+					cell.Style.Font.UnderLine = true;
+				}
+
+				if (!string.IsNullOrEmpty(resultSheet.Cells[i, Result.SolutionUriColumnIndex].Text))
+				{
+					cell = resultSheet.Cells[i, Result.SolutionColumnIndex];
+					cell.Hyperlink = new Uri(resultSheet.Cells[i, Result.SolutionUriColumnIndex].Text);
+					cell.Style.Font.Color.SetColor(Color.Blue);
+					cell.Style.Font.UnderLine = true;
+				}
+			}
+
+			resultSheet.Column(Result.SolutionUriColumnIndex).Hidden = true;
+			resultSheet.Columns.AutoFit();
+
+			return excelPackage.GetAsByteArray();
+		}
+	}
+}
